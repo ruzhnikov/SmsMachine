53471159ed188aa426966f01c83f499ecbc47a42
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2015-04-09 22:05:44 +0300

    * replaced Modern::Perl and added new methods to DB*

--- lib/SmsMachine/DB.pm
+++ lib/SmsMachine/DB.pm
@@ -1,8 +1,15 @@
 package SmsMachine::DB;
 
-use Modern::Perl;
+use strict;
+use warnings;
+use 5.010;
 use DBI;
 use Carp qw/ croak /;
+
+use constant {
+    QUERY_LIMIT  => 10,  # limit for queries
+    QUERY_OFFSET => 0,
+};
 
 use base qw/ SmsMachine::DB::Requests /;
 
@@ -15,11 +22,27 @@ sub new {
     }
 
     my $self = bless { _db_file => $file }, $class;
-    $self->{_dbh} = DBI->connect( "dbi:SQLite:dbname=$file", '', '' );
+    my $add_params = {
+                RaiseError     => 1,
+                sqlite_unicode => 1,
+    };
+    $self->{_dbh} = DBI->connect( "dbi:SQLite:dbname=$file", '', '', $add_params ) or die DBI->errstr;
+    $self->{_query_limit}  = QUERY_LIMIT;
+    $self->{_query_offset} = QUERY_OFFSET;
+
+    $self->{_task_table} = 'tasks';
+    $self->{_msg_table}  = 'task_messages';
+    $self->{_num_table}  = 'task_numbers';
 
     return $self;
 }
 
 sub dbh { shift->{_dbh} }
+sub limit { shift->{_query_limit} }
+sub offset { shift->{_query_offset} }
+sub task_table { shift->{_task_table} }
+sub msg_table { shift->{_msg_table} }
+sub num_table { shift->{_num_table} }
+
 
 1;
--- lib/SmsMachine/DB/Requests.pm
+++ lib/SmsMachine/DB/Requests.pm
@@ -1,11 +1,143 @@
 package SmsMachine::DB::Requests;
 
-use Modern::Perl;
+=head1 NAME
+
+SmsMachine::DB::Requests
+
+=head1 DESCRIPTION
+
+Requests to database
+
+=head1 SYNOPSIS
+
+my $dbh   = SmsMachine::DB->new;
+my $tasks = $dbh->get_tasks;
+
+for my $task ( @{ $tasks } ) {
+    say $task->{id} . ": " . $task->{status};
+}
+
+=head1 AUTHOR
+
+Alexander Ruzhnikov <ruzhnikov85@gmail.com>
+
+=cut
+
+use strict;
+use warnings;
+use 5.010;
+
+=head1 METHODS
+
+=over
+
+=item B<get_tasks_query>( $self, %param )
+
+=cut
+
+sub get_tasks_query {
+    my ( $self, %param ) = @_;
+
+    my (@bind_params, @wherecond);
+    my ($fields, $wherecond);
+
+    if ( $param{status} ) {
+        for my $status ( @{ $param{status} } ) {
+            push @wherecond, 'status = ?';
+            push @bind_params, $status;
+        }
+    }
+
+    if ( $param{fields} ) {
+        $fields = join( ', ', @{ $param{fields} } );
+    }
+
+    $wherecond = join( ' OR ', @wherecond ) if ( scalar @wherecond );
+
+    if ( $param{id} ) {
+        $wherecond .= ' AND ' if $wherecond;
+        if ( scalar @{ $param{id} } == 1 ) {
+            $wherecond .= 'id = ?';
+            push @bind_params, $param{id}->[0];
+        }
+        else {
+            $wherecond .= 'id IN ( ' . join( ', ', @{ $param{id} } ) . ' )';
+        }
+    }
+
+    my $query = qq/SELECT / .
+                ( $fields ? $fields : 'id' ) .
+                qq/ FROM / . $self->task_table .
+                ( $wherecond ? qq/ WHERE $wherecond / : '' ) .
+                qq/ LIMIT 0,/ . $self->limit;
+
+    return $self->dbh->selectall_arrayref( $query, { Slice => {} }, @bind_params );
+}
 
 sub get_tasks {
-    my ( $self, %params ) = @_;
+    my ( $self ) = @_;
 
-    # ...
+    return $self->get_tasks_query(
+        status => [ qw/ new running / ],
+        fields => [ qw/ id status / ],
+    );
 }
+
+sub get_task_info {
+    my ( $self, $task_id, $fields ) = @_;
+
+    $fields ||= '*';
+
+    return $self->get_tasks_query(
+        fields => [ $fields ],
+        id     => [ $task_id ],
+    );
+}
+
+sub get_new_tasks {
+    my ( $self ) = @_;
+
+    return $self->get_tasks_query(
+        status => [ qw/ new / ],
+        fields => [ qw/ id / ],
+    );
+}
+
+sub get_run_tasks {
+    my ( $self ) = @_;
+
+    return $self->get_tasks_query(
+        status => [ qw/ running / ],
+        fields => [ qw/ id / ],
+    );
+}
+
+sub get_suc_tasks {
+    my ( $self ) = @_;
+
+    return $self->get_tasks_query(
+        status => [ qw/ success / ],
+        fields => [ qw/ id / ],
+    );
+}
+
+sub get_fail_tasks {
+    my ( $self ) = @_;
+
+    return $self->get_tasks_query(
+        status => [ qw/ fail / ],
+        fields => [ qw/ id / ],
+    );
+}
+
+sub get_task_date_start {
+    my ( $self, $task_id ) = @_;
+
+    return $self->get_task_info( $task_id, 'date_start' );
+}
+
+=back
+
+=cut
 
 1;
